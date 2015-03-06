c5bb883fb753c32fde11d0d6ccfb4cd2bcb5e7ae
Tully Foote <tfoote@osrfoundation.org>
2015-03-06 15:18:53 -0800

    moving checkout inside docker since vcs is not on the build slaves.

--- run_test.bash
+++ run_test.bash
@@ -3,8 +3,7 @@
 # Fetch the sources to build
 WS_PATH=`pwd`/ros2_ws
 mkdir -p $WS_PATH
-curl --silent https://raw.githubusercontent.com/ros2/examples/pre_api_review/ros2.repos -o $WS_PATH/ros2.repos
-vcs import $WS_PATH/src < $WS_PATH/ros2.repos
+
 
 docker build -t ros2_test .
 docker run -ti -v $WS_PATH:/root/ros2_ws ros2_test
--- test.bash
+++ test.bash
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 
-cd ~/ros2_ws/
+WS_PATH=/root/ros2_ws
+mkdir -p $WS_PATH/src
+
+curl --silent https://raw.githubusercontent.com/ros2/examples/pre_api_review/ros2.repos -o $WS_PATH/ros2.repos
+vcs import $WS_PATH/src < $WS_PATH/ros2.repos
+
+cd $WS_PATH
 src/ament/ament_tools/scripts/ament.py build --build-tests --symlink-install
 src/ament/ament_tools/scripts/ament.py test src
 src/ament/ament_tools/scripts/ament.py test_results
