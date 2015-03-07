cd4fd48ea0c9c5b32d6505e0adc55627423a16e4
Tully Foote <tfoote@osrfoundation.org>
2015-03-06 17:23:51 -0800

    move Dockerfile into subdirectory to not pass the whole workspace to docker daemon, and loosen errexit

--- docker_context/test.bash
+++ docker_context/test.bash
@@ -8,6 +8,9 @@ mkdir -p $WS_PATH/src
 curl --silent https://raw.githubusercontent.com/ros2/examples/pre_api_review/ros2.repos -o $WS_PATH/ros2.repos
 vcs import $WS_PATH/src < $WS_PATH/ros2.repos
 
+# don't error on build failure?
+set +o errexit
+
 cd $WS_PATH
 src/ament/ament_tools/scripts/ament.py build --build-tests --symlink-install
 src/ament/ament_tools/scripts/ament.py test src
--- run_test.bash
+++ run_test.bash
@@ -7,5 +7,5 @@ WS_PATH=`pwd`/ros2_ws
 mkdir -p $WS_PATH
 
 
-docker build -t ros2_test .
-docker run -v $WS_PATH:/root/ros2_ws ros2_test
+docker build -t ros2_test docker_context
+docker run -i -v $WS_PATH:/root/ros2_ws ros2_test
