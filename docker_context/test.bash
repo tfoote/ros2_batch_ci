d1e0270f45740666a97ccf95a8eae0ed9c1872b3
Tully Foote <tfoote@osrfoundation.org>
2015-03-09 13:24:07 -0700

    switch to master fixes #4

--- docker_context/test.bash
+++ docker_context/test.bash
@@ -5,12 +5,8 @@ set -o errexit
 WS_PATH=/home/rosbuild/ros2_ws
 mkdir -p $WS_PATH/src
 
-curl --silent https://raw.githubusercontent.com/ros2/examples/pre_api_review/ros2.repos -o $WS_PATH/ros2.repos
+curl --silent https://raw.githubusercontent.com/ros2/examples/master/ros2.repos -o $WS_PATH/ros2.repos
 vcs import $WS_PATH/src < $WS_PATH/ros2.repos
-
-# don't error on build failure?
-set +o errexit
-
 
 
 cd $WS_PATH
@@ -20,4 +16,4 @@ rm -rf build devel install
 
 src/ament/ament_tools/scripts/ament.py build --build-tests
 src/ament/ament_tools/scripts/ament.py test src
-src/ament/ament_tools/scripts/ament.py test_results
+src/ament/ament_tools/scripts/ament.py test_results || true
